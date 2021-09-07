6b2c14ab88cb64960e275e6660b9881feaf0d971
YOONHEECHEOL <luk2903201@gmail.com>
2021-09-08 01:02:29 +0900

    쿵쿵따 만들어보기

--- js/main.js
+++ js/main.js
@@ -1,20 +1,64 @@
 const number = parseInt(prompt('총 몇명의 플레이어가 참가하나요?'))
 console.log('입력받은 플레이어 수 : ', number)
+const word = document.querySelector('#word'); // 제시어
+const inputText = document.querySelector('input') // 입력된 단어
+const inputBtn = document.querySelector('button') // 입력 버튼
+const guideWord = String(word.textContent)
+let newWord; // 새로입력한 단어
 
-const list = document.querySelector('.list');
-const order = document.querySelector('#order');
-const start = document.querySelector('.start');
-const word = document.querySelector('#word');
-let input = document.querySelector('input');
-const btn = document.querySelector('button');
 
-function insertWord() {
-  word.textContent = input.textContent;
-  input.textContent = '';
+function lengthCheck(input) { // 글자 길이 체크
+  const checkWord = String(input)
+  if (checkWord.length === 3) {
+    return true;
+  } else {
+    alert('글자 수가 맞지않습니다! 3글자를 맞춰주세요!')
+  }
+}
+// console.log(lengthCheck(word))
+
+function wordCheck(input) { // 끝말잇기 체크
+  const checkWord = String(input)
+
+  if (checkWord[0] === word.textContent[2]) {
+    let inputWord = word.textContent
+    console.log('word : ', inputWord)
+    return true;
+  } else {
+    let inputWord = word.textContent
+    console.log('word : ', inputWord)
+    alert('끝말이 이어지지 않습니다!')
+  }
 }
 
-if (!word) {
-  btn.addEventListener('click',)
-} else {
+// console.log(wordCheck('실타래'));
 
+// 1. 제시어가 비어있음?
+// 2. 검증1 - 입력한 단어의 글자 수 체크 (3글자)
+// 3. 검증2 - 입력한 단어가 끝말잇기가 되었는지 체크
+const onInput = (event) => {
+  newWord = event.target.value
 }
+
+const onClickBtn = () => {
+
+  if (!newWord) { // 제시어가 비어있음    
+    if (lengthCheck(newWord)) {
+      console.log(newWord)
+      word.textContent = newWord
+    } else {
+      alert('제시어 채우기!')
+    }
+    console.log('sucess!')
+  } else { // 제시어가 비어있지 않음
+    if (lengthCheck(newWord) && wordCheck(newWord)) {
+      word.textContent = newWord
+    } else {
+      alert('뷁!')
+    }
+    console.log('error!')
+  }
+}
+
+inputBtn.addEventListener('click', onClickBtn)
+inputText.addEventListener('input', onInput)
